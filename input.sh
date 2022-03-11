bb8e712ba56fb24029d71cb889e952aeaba2d765
sreeni <seena.dsr@gmail.com>
2022-03-11 16:16:46 +0800

    new input script1

--- input.sh
+++ input.sh
@@ -3,3 +3,11 @@
 read -p "Enter youname = " name
 
 echo "your name is = $name"
+
+#special variables
+
+ echo "the script name is = $0"
+ echo " print my first number = $1"
+ echo " print my second number =$2"
+ echo "print my all number = $*"
+ echo "print my total numbers = $#"
