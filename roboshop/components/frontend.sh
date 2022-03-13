c069c5a033124213eb0c014289b6315e20793d6f
sreeni <seena.dsr@gmail.com>
2022-03-13 15:27:37 +0800

    frondend auto file2

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -15,6 +15,8 @@ else
   echo -e " \e[31mInstallation should run with only root\e[0m ";exit 2
 fi
 
+LOG_FILE=/tmp/roboshop.log
+rm -rf $LOG_FILE
 print() {
   echo -e "\e[36m $1 \e[0m"
 }
@@ -24,29 +26,29 @@ space () {
   echo "======================================"
 }
 print "Installing nginx"
-yum install nginx -y
+yum install nginx -y >>$LOG_FILE
 StatCheck $? "Nginx Installation - "
 space
 print "Copying frontend config files"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >> $LOG_FILE
 StatCheck $? "Curl copy - "
 space
 print " cleaning up old-files"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 StatCheck $? "cleaning up files -"
 space
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html >>$LOG_FILE
 print "extracting files "
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
+unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
 StatCheck $? "extracting files - "
 space
 
 print " Configuring roboshop files"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
 StatCheck $? "Configuring roboshop - "
 space
 
 print "starting nginx services"
-systemctl restart nginx && systemctl enable nginx && systemctl status nginx|grep active
+systemctl restart nginx >>$LOG_FILE && systemctl enable nginx >>$LOG_FILE && systemctl status nginx|grep active >>$LOG_FILE
 StatCheck $? " Nginx status - "
