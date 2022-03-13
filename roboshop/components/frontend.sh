f2c157e373657a43e56e1c8f7b463d0c3f31cb67
sreeni <seena.dsr@gmail.com>
2022-03-13 15:42:17 +0800

    frondend auto file2

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -18,37 +18,34 @@ fi
 LOG_FILE=/tmp/roboshop.log
 rm -rf $LOG_FILE
 print() {
+  echo " ===================================="
   echo -e "\n================$1=============" >>$LOG_FILE
   echo -e "\e[36m $1 \e[0m"
 }
 
-space () {
-  echo "  "
-  echo "======================================"
-}
 print "Installing nginx"
 yum install nginx -y >>$LOG_FILE
 StatCheck $? "Nginx Installation - "
-space
+
 print "Copying frontend config files"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >> $LOG_FILE
 StatCheck $? "Curl copy - "
-space
+
 print " cleaning up old-files"
 rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 StatCheck $? "cleaning up files -"
-space
+
 
 cd /usr/share/nginx/html >>$LOG_FILE
 print "extracting files "
 unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
 StatCheck $? "extracting files - "
-space
+
 
 print " Configuring roboshop files"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
 StatCheck $? "Configuring roboshop - "
-space
+
 
 print "starting nginx services"
 systemctl restart nginx >>$LOG_FILE && systemctl enable nginx >>$LOG_FILE && systemctl status nginx|grep active >>$LOG_FILE
