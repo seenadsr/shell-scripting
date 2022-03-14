42a17210da55d1ef3ffa5dba754b8783ee536c20
sreeni <seena.dsr@gmail.com>
2022-03-14 10:44:56 +0800

    frondend auto file2

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,17 +3,17 @@
 source components/common.sh
 
 print "MongoDB YUM Repo setup"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo >>$LOG_FILE
 StatCheck $? "MongoDB repository - "
 
 print " MongoDB installation "
-yum install -y mongodb-org
+yum install -y mongodb-org >>$LOG_FILE
 StatCheck $? " MongoDB Installation success"
 
 
 print " starting and enabling mongodb services"
-systemctl enable mongod
-systemctl start mongod
+systemctl enable mongod >>$LOG_FILE && systemctl start mongod >>$LOG_FILE
+
 
 
 
