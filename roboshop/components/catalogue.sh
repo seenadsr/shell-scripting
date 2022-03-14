710ae881a22b3743f4a347c6b8c1b3f17270367c
sreeni <seena.dsr@gmail.com>
2022-03-14 17:17:49 +0800

    frondend auto file3

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,45 +1,7 @@
 #!/bin/bash
 source components/common.sh
 
-print " Setting up catalouge YUM repo "
-curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - >> $LOG_FILE
-StatCheck $? " Repo downloaded -  "
+COMPONENT=catalogue
 
-print " Installing nodejs  "
-yum install nodejs gcc-c++ -y >>$LOG_FILE
-StatCheck $? " Installation nodejs - "
-
-print " Adding application user roboshop "
-id ${APP_USER} >>$LOG_FILE
-if [ $? -ne 0 ];then
-useradd ${APP_USER} >> $LOG_FILE
-fi
-StatCheck $? " Application user adding  - "
-
-print " Download nodejs config file "
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" >>$LOG_FILE
-StatCheck $? "Downloading nodejs config files  - "
-
-print "Cleaning old files "
-rm -rf /home/${APP_USER}/catalogue >>$LOG_FILE
-StatCheck $? " Old files cleanup -"
-
-print " extract and configure nodejs"
-cd /home/roboshop >>$LOG_FILE && unzip -o /tmp/catalogue.zip >>$LOG_FILE && mv -f catalogue-main catalogue >>$LOG_FILE && cd /home/roboshop/catalogue >>$LOG_FILE
-StatCheck $? " nodejs configuration - "
-
-print " npm installation "
-npm install >>$LOG_FILE
-StatCheck $? " npm installation success "
-
-print " Changing permission for application user"
-chown -R ${APP_USER}:${APP_USER} /home/roboshop >>$LOG_FILE
-StatCheck $? " Permission changed - "
-
-print "update mnagodb DNS name"
-sed -i -e 's/MONGO_DNSNAME/mongo.roboshop.internal/' /home/roboshop/catalogue/systemd.service >>$LOG_FILE
-StatCheck $? " MangoDB DNS name updated - "
-
-print " Start systemd services "
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service >>$LOG_FILE && systemctl daemon-reload >>$LOG_FILE && systemctl start catalogue >>$LOG_FILE  && systemctl enable catalogue >>$LOG_FILE
-StatCheck $? " services started "
+#Function
+NODJS
