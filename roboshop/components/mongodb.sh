c6fb01bae1f8216ec7dad7c82240241ec824954e
sreeni <seena.dsr@gmail.com>
2022-03-14 11:07:23 +0800

    frondend auto file2

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -15,7 +15,7 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 StatCheck $? " MongoDB lister address configure - "
 
 print " starting and enabling mongodb services"
-systemctl enable mongod >>$LOG_FILE && systemctl start mongod >>$LOG_FILE
+systemctl enable mongod >>$LOG_FILE && systemctl restart mongod >>$LOG_FILE
 StatCheck $? " service starting is -  "
 
 
