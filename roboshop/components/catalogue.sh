cd00bddeb30a7f83a4a16a7b74afc951f08f2205
sreeni <seena.dsr@gmail.com>
2022-03-14 14:49:14 +0800

    frondend auto file3

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -6,7 +6,7 @@ curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - >> $LOG_FILE
 StatCheck $? " Repo downloaded -  "
 
 print " Installing nodejs  "
-yum install nodejs gcc-c++ -y >>LOG_FILE
+yum install nodejs gcc-c++ -y >>$LOG_FILE
 StatCheck $? " Installation nodejs - "
 
 print " Adding application user roboshop "
@@ -17,21 +17,21 @@ fi
 StatCheck $? " Application user adding  - "
 
 print " Download nodejs config file "
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" >>LOG_FILE
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" >>$LOG_FILE
 StatCheck $? "Downloading nodejs config files  - "
 
 print "Cleaning old files "
-rm -rf /home/${APP_USER}/catalogue >>LOG_FILE
+rm -rf /home/${APP_USER}/catalogue >>$LOG_FILE
 StatCheck $? " Old files cleanup -"
 
 print " extract and configure nodejs"
-cd /home/roboshop >>LOG_FILE && unzip -o /tmp/catalogue.zip >>LOG_FILE && mv -f catalogue-main catalogue >>LOG_FILE && cd /home/roboshop/catalogue >>LOG_FILE
+cd /home/roboshop >>$OG_FILE && unzip -o /tmp/catalogue.zip >>$LOG_FILE && mv -f catalogue-main catalogue >>$LOG_FILE && cd /home/roboshop/catalogue >>$LOG_FILE
 StatCheck $? " nodejs configuration - "
 
 print " npm installation "
-npm install >>LOG_FILE
+npm install >>$LOG_FILE
 StatCheck $? " npm installation success "
 
 print " Changing permission for application user"
-chown -R ${APP_USER}:${APP_USER} /home/roboshop >>LOG_FILE
+chown -R ${APP_USER}:${APP_USER} /home/roboshop >>$LOG_FILE
 StatCheck $? " Permission changed - "
