a1425ee57535cb4008eac5998f34063ac82f4fc7
sreeni <seena.dsr@gmail.com>
2022-03-14 17:13:58 +0800

    frondend auto file3

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -23,3 +23,47 @@ print() {
 }
 
 APP_USER=roboshop
+NODJS() {
+  print " Setting up YUM repo "
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - >> $LOG_FILE
+  StatCheck $? " Repo downloaded -  "
+
+  print " Installing nodejs  "
+  yum install nodejs gcc-c++ -y >>$LOG_FILE
+  StatCheck $? " Installation nodejs - "
+
+  print " Adding application user roboshop "
+  id ${APP_USER} >>$LOG_FILE
+  if [ $? -ne 0 ];then
+  useradd ${APP_USER} >> $LOG_FILE
+  fi
+  StatCheck $? " Application user adding  - "
+
+  print " Download nodejs config file "
+  curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" >>$LOG_FILE
+  StatCheck $? "Downloading nodejs config files  - "
+
+  print "Cleaning old files "
+  rm -rf /home/${APP_USER}/${COMPONENT} >>$LOG_FILE
+  StatCheck $? " Old files cleanup -"
+
+  print " extract and configure nodejs"
+  cd /home/roboshop >>$LOG_FILE && unzip -o /tmp/${COMPONENT}.zip >>$LOG_FILE && mv -f ${COMPONENT}-main ${COMPONENT} >>$LOG_FILE && cd /home/roboshop/${COMPONENT} >>$LOG_FILE
+  StatCheck $? " nodejs configuration - "
+
+  print " npm installation "
+  npm install >>$LOG_FILE
+  StatCheck $? " npm installation success "
+
+  print " Changing permission for application user"
+  chown -R ${APP_USER}:${APP_USER} /home/roboshop >>$LOG_FILE
+  StatCheck $? " Permission changed - "
+
+  print "update DNS name"
+  sed -i -e 's/MONGO_DNSNAME/mongo.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' /home/roboshop/catalogue/systemd.service >>$LOG_FILE
+  StatCheck $? " DNS name updated - "
+
+  print " Start systemd services "
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service >>$LOG_FILE && systemctl daemon-reload >>$LOG_FILE && systemctl start ${COMPONENT} >>$LOG_FILE  && systemctl enable ${COMPONENT} >>$LOG_FILE
+  StatCheck $? " services started "
+}
