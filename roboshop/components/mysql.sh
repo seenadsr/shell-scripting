b78e47430f41aa6dddf9a010d856539a8eba1eba
sreeni <seena.dsr@gmail.com>
2022-03-20 12:04:53 +0800

    frondend auto file3

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -22,7 +22,7 @@ StatCheck $?
 fi
 
 echo "show plugins"|mysql -uroot -pRoboShop@1|grep validate_password 2>>${LOG_FILE}
-if [ $? -eq o ];then
+if [ $? -eq 0 ];then
 print "Uninstall plugin validate_password"
 echo "uninstall plugin validate_password;" >/tmp/plugin.sql
 mysql --connect-expired-password -uroot -pRoboShop@1 < /tmp/plugin.sql
