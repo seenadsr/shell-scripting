be7a9338c5acecafb239476aab392c53b496f292
sreeni <seena.dsr@gmail.com>
2022-03-20 15:34:50 +0800

    frondend auto file3

---
+++ roboshop/components/rabittmq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source components/common.sh
+
+
+print "Setup YUM repositories for RabbitMQ"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash >>${LOG_FILE}
+StatCheck $?
+
+print " Install erlang & rabittmq"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y >>${LOG_FILE}
+yum install rabbitmq-server -y >>${LOG_FILE}
+StatCheck $?
+
+print " start rabittmq services "
+systemctl enable rabbitmq-server >>${LOG_FILE} && systemctl start rabbitmq-server >>${LOG_FILE}
+
+print "Create application user"
+rabbitmqctl add_user roboshop roboshop123 >>${LOG_FILE}
+
+## rabbitmqctl set_user_tags roboshop administrator
+## rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+#```
+#
+#Ref link : [https://www.rabbitmq.com/rabbitmqctl.8.html#User_Management](https://www.rabbitmq.com/rabbitmqctl.8.html#User_Management)
