1f6973a191c04487a1c60f69f20ad5769fdc54cb
sreeni <seena.dsr@gmail.com>
2022-03-21 15:34:46 +0800

    frondend auto file3

--- roboshop/ec2-instance.sh
+++ roboshop/ec2-instance.sh
@@ -5,6 +5,7 @@ if [ -z $1 ];then
   exit 1
 fi
 COMPONENT=$1
+HZ_ID=Z05655191AJ3UQ0VAI8J0
 AMI_ID=$(aws ec2 describe-images --filter Name=name,Values=Centos-7-DevOps-Practice --query 'Images[].ImageId' --output text)
 SG_ID=$(aws ec2 describe-security-groups --filter "Name=group-name,Values=launch-wizard-3"|jq ."SecurityGroups[].GroupId"|sed -e 's/"//g')
 
@@ -17,3 +18,4 @@ PRIVATE_IP=$(aws ec2 run-instances \
 |jq ."Instances[].PrivateIpAddress" |sed -e 's/"//g')
 
 sed -e "s/PRIVATE_IP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" sample.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ${HZ_ID} --change-batch file:///tmp/record.json
