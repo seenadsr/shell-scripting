e0ed0613196939de6b56acf9a4d380fb5df8ec0e
sreeni <seena.dsr@gmail.com>
2022-03-21 18:44:30 +0800

    frondend auto file3

--- roboshop/terminate-ec2.sh
+++ roboshop/terminate-ec2.sh
@@ -5,11 +5,11 @@ SPOT_REQ=$(aws ec2 describe-instances --filters Name=instance-lifecycle,Values=s
 
 echo ${SPOT_EC2}
 if [ $? -eq 0 ];then
-   aws ec2 terminate-instances --instance-ids $SPOT_EC2
+   aws ec2 terminate-instances --instance-ids $SPOT_EC2|jq
   fi
 
 echo ${SPOT_REQ}
  if [ $? -eq 0 ];then
-    aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${SPOT_REQ}
+    aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${SPOT_REQ}|jq
   fi
 
