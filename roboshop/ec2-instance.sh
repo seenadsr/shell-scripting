d839ff76cf1814f27347c12fa08fc163379442ab
sreeni <seena.dsr@gmail.com>
2022-03-21 14:22:55 +0800

    frondend auto file3

--- roboshop/ec2-instance.sh
+++ roboshop/ec2-instance.sh
@@ -6,4 +6,8 @@ if [ -z $1 ];then
 fi
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filter Name=name,Values=Centos-7-DevOps-Practice --query 'Images[].ImageId' --output text)
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+SG_ID=$(aws ec2 describe-security-groups --filter "Name=group-name,Values=launch-wizard-3"|jq ."SecurityGroups[].GroupId"|sed -e 's/"//g')
+
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro \
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+--security-group-ids ${SG_ID}
