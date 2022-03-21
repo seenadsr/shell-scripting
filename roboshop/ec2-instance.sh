bf99670f111a4593b07a5c5e1d608a559a3c6ddd
sreeni <seena.dsr@gmail.com>
2022-03-21 18:10:43 +0800

    frondend auto file3

--- roboshop/ec2-instance.sh
+++ roboshop/ec2-instance.sh
@@ -34,13 +34,4 @@ else
     create-ec2
 fi
 
-#spot instance removal
-SPOT_EC2=$(aws ec2 describe-instances --filters Name=instance-lifecycle,Values=spot --query Reservations[*].Instances[*].[InstanceId] --output text)
-SPOT_REQ=$(aws ec2 describe-instances --filters Name=instance-lifecycle,Values=spot --query Reservations[*].Instances[*].[SpotInstanceRequestId] --output text)
-
-if [ "$1" == "del" ];then
-  for terminate in ${SPOT_EC2};do
-    COMPONENT=$terminate
-       aws ec2 terminate-instances --instance-ids ${COMPONENT}
-fi
 
---
+++ roboshop/terminate-ec2.sh
@@ -0,0 +1,25 @@
+
+if [ -z "$1" ];then
+  echo " Instance name need to be specified"
+  exit 1
+fi
+
+ COMPONENT=$1
+
+#spot instance removal
+SPOT_EC2=$(aws ec2 describe-instances --filters Name=instance-lifecycle,Values=spot --query Reservations[*].Instances[*].[InstanceId] --output text)
+SPOT_REQ=$(aws ec2 describe-instances --filters Name=instance-lifecycle,Values=spot --query Reservations[*].Instances[*].[SpotInstanceRequestId] --output text)
+
+if [ "$1 == "del" ];then
+   for terminate in ${SPOT_EC2}; do
+   COMPONENT=$terminate
+   aws ec2 terminate-instances --instance-ids ${COMPONENT}
+   done
+elif [ "$1" == "cancel" ];then
+   for cancel in ${SPOT_REQ}; do
+    COMPONENT=$cancel
+    aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${COMPONENT}
+ done
+
+fi
+
