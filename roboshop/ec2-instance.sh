bbec47bdb741ba38d3133b552caf63e64d2f4982
sreeni <seena.dsr@gmail.com>
2022-03-21 17:15:16 +0800

    frondend auto file3

--- roboshop/ec2-instance.sh
+++ roboshop/ec2-instance.sh
@@ -36,10 +36,9 @@ fi
 SPOT_EC2=$(aws ec2 describe-instances --filters Name=instance-lifecycle,Values=spot --query Reservations[*].Instances[*].[InstanceId] --output text)
 SPOT_REQ=$(aws ec2 describe-instances --filters Name=instance-lifecycle,Values=spot --query Reservations[*].Instances[*].[SpotInstanceRequestId] --output text)
 
-if [ "$1" == "del"];then
+if [ "$1" == "del" ];then
   for terminate in ${SPOT_EC2};do
     COMPONENT=$terminate
        aws ec2 terminate-instances --instance-ids ${COMPONENT}
-
 fi
 
