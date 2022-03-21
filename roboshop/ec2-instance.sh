f94534a8d88b943b6d37b395351cc266df592cd4
sreeni <seena.dsr@gmail.com>
2022-03-21 13:51:56 +0800

    frondend auto file3

--- roboshop/ec2-instance.sh
+++ roboshop/ec2-instance.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ -z $1 ];then
+  echo "\e{31mInstance name to be specified\e[0m"
+  exit 1
+fi
+COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filter Name=name,Values=Centos-7-DevOps-Practice --query 'Images[].ImageId' --output text)
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}}]'
